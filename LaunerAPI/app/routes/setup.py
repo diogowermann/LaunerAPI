694550e1d83c2dfb9c7d65e34072692c8d115456
diogowermann <wermanndiogo@gmail.com>
2025-04-07 17:07:12 -0300

    Corrected setup.py file to use APIRouter

--- LaunerAPI/app/routes/setup.py
+++ LaunerAPI/app/routes/setup.py
@@ -1,18 +1,40 @@
-from fastapi import Depends
+# setup.py
+from fastapi import APIRouter, Depends
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from app.database import get_protheus_db
-from app.core.security import LoginData
-from app.core.security import authenticate_user
+from app.core.security import LoginData, authenticate_user, create_access_token
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import timedelta
 
-def setup_routes(app):
-    """Setup all routes for aplication."""
-    @app.get("/")
-    async def get_app():
-        return {"message": "FastAPI running"}
+router = APIRouter()
 
-    @app.post("/login")
-    async def login(login_data: LoginData, db: Session = Depends(get_protheus_db)):
-        """Login user."""
-        user = authenticate_user(db, login_data.username, login_data.password)
-        return {"message": "Login realizado com sucesso!"}    
-        
+@router.get("/")
+async def get_app():
+    return {"message": "FastAPI running"}
+
+@router.post("/login")
+async def login(login_data: LoginData, db: Session = Depends(get_protheus_db)):
+    user = authenticate_user(db, login_data.username, login_data.password)
+    access_token = create_access_token(
+        data={"sub": user.api_usr_codigo},
+        expires_delta=timedelta(minutes=30),
+    )
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user": user.api_usr_codigo
+    }
+
+def setup_middleware(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+def setup_static_files(app):
+    app.mount("/static", StaticFiles(directory="frontend/build/static"), name="static")
+    app.mount("/", StaticFiles(directory="frontend/build", html=True), name="react")
